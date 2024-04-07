5ced24eeb0d21c5cda7b9e2eb2fc5b3619766c01
Esperlluette <janl.mannessier@gmail.com>
2024-04-07 11:49:06 +0000

    display message ok

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,24 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Messages;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 class MessageController extends AbstractController
 {
     #[Route('/message', name: 'app_message')]
     public function index(EntityManagerInterface $em): Response
     {
 
-        if ($_GET==null){
+        if ($_GET == null) {
             return $this->redirectToRoute('app_list_category');
         }
 
-        
+        try {
+            $userid = $this->getUser()->getId() ?? 0;
+        } catch (\Throwable $th) {
+            $userid = 0;
+        }
+
+        $message = $em->getRepository(Messages::class)->findById($_GET["msg"]);
         return $this->render('message/index.html.twig', [
             'controller_name' => 'MessageController',
+            'message' => $message,
+            'userid' => $userid,
         ]);
     }
 }
--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -11,8 +11,12 @@ class ProfileController extends AbstractController
     #[Route('/profile', name: 'app_profile')]
     public function index(): Response
     {
+        $user = $this->getUser();
+
+        // dd($user);
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
+            'user'=>$user
         ]);
     }
 }
